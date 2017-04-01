d3d9d369c853e9ee1c12c63612e8c9918ff2871a
Dan Buda <danbuda@gmail.com>
2017-04-01 17:38:20 -0500

    weather fetch working

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators} from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = { term: '' };
 
 		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 	onInputChange(e) {
@@ -18,6 +22,9 @@ export default class SearchBar extends Component {
 		e.preventDefault();
 
 		// Fetch weather data
+		this.props.fetchWeather(this.state.term);
+		// Clear the search input after submit
+		this.setState({ term: '' });
 	}
 
 	render() {
@@ -35,3 +42,10 @@ export default class SearchBar extends Component {
 		);
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// First argument is null because we're not concerned about state
+export default connect(null, mapDispatchToProps)(SearchBar);
